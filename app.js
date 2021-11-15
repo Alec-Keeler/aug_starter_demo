602bdaeb0b0943d14fc4d05a8ba6cfef225163b9
Alec Keeler <alec.keeler@gmail.com>
2021-11-15 10:57:31 -0600

    reset changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { sequelize } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
-const bananasRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -36,7 +36,7 @@ app.use(
 store.sync();
 
 app.use('/', indexRouter);
-app.use('/bananas', bananasRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
