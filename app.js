4e2b1feb5ed81c015945fd8fbb2fde6ffe1ce910
nathanieldcooke <nathanield.cooke@gmail.com>
2021-11-15 10:14:38 -0800

    update indexRouter to appleRouter

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const logger = require('morgan');
 const { sequelize } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const appleRouter = require('./routes/index');
+const bananaRouter = require('./routes/users');
 
 const app = express();
 
@@ -35,8 +35,8 @@ app.use(
 // create Session table if it doesn't already exist
 store.sync();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/apple', appleRouter);
+app.use('/banana', bananaRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
